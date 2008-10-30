96e06c3d436ec78f4cba94c20d4ecaae8632fc73
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2008-10-30 10:50:25 +0000

    Function getCodeStored() was change into class modJumiHelper behavior. Prevents conflicts in multiple instances of module.

--- module_J15/tmpl/default.php
+++ module_J15/tmpl/default.php
@@ -5,17 +5,7 @@
 * @copyright (c) 2008 Martin Hajek
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
 */
-
-// no direct access
 defined('_JEXEC') or die('Restricted access');
-
-function getCodeStored($source){ //returns code stored in the database or null.
-	$database  = &JFactory::getDBO();
-	//$user      = &JFactory::getUser();
-	//$database->setQuery("select custom_script from #__jumi where id = '{$source}' and access <= {$user->gid} and published = 1");
-	$database->setQuery("select custom_script from #__jumi where id = $source");
-	return $database->loadResult();
-}
 
 if ($code_written.$storage_source != ""){ //something to show
   if ($code_written != ""){ //if code written
@@ -23,9 +13,8 @@ if ($code_written.$storage_source != ""){ //something to show
 	}
 	if ($storage_source != ""){ // if record id or filepathname
 		if (is_int($storage_source)){ //it is record id
-			$code_written = getCodeStored($storage_source);
-			if ($code_written != null) {
-				eval ('?>'.$code_written); //include custom script written
+			if ($code_stored != null) {
+				eval ('?>'.$code_stored); //include custom script written
 			}
 			else {
 				echo '<div style="color:#FF0000;background:#FFFF00;">'.JText::sprintf('ERROR_RECORD',$storage_source).'</div>';
