0c54a56f23abc9429fd56165c243582268081147
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2008-11-03 22:28:21 +0000

    Deleted function that could cause conflicts. Improved the advertising tone.

--- module_J15/jumi_demo.php
+++ module_J15/jumi_demo.php
@@ -6,15 +6,10 @@
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
 */  
 defined('_JEXEC') OR die( "Direct Access Is Not Allowed" );
-function sitename() { //gets sitename
- $config = new JConfig();
- return $config->sitename;
-}
 ?>
 
-<!-- Intro -->
 <p><strong>Hello in the world of Jumi!</strong></p>
-<p>This is the default Jumi demo file presented on <b><?php echo sitename(); ?></b>.</p>
-<p>Your php codes can be made fully reusable like functions with Jumi extensions. E.g. values of variables of your stored codes can be set up in an appropriate "code" area in Jumi module administration.</p>
-<p>You can also try Jumi component and plugin that are compatible with this module.</p>
+<p>This is the default Jumi demo file presented on <b><?php echo $mainframe->getCfg('live_site'); ?></b>.</p>
+<p>Jumi is a set of custom code extensions for CMS Joomla! Jumi comes as a component, a plugin and a module.</p>
+<p>Jumi brings you sufficient power, flexibility and simplicity for quick development and reliable operation of your applications running under Joomla!</p>
 <p>Jumi resources: <a href="http://jumi.vedeme.cz">Downloads & guides</a>, <a href="http://edo.webmaster.am/jumi">Tips & tricks</a>.</p>
