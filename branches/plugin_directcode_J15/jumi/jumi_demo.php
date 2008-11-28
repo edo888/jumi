6d059900c69fc88e10a038f7cad7e15e3486eac5
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2008-11-28 10:48:44 +0000

    Plugin with possibility of writing the code into the articles directly

---
+++ branches/plugin_directcode_J15/jumi/jumi_demo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @version $Id$
+* @package Jumi
+* @copyright (C) 2008 Martin Hajek
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+*/  
+defined('_JEXEC') OR die( "Direct Access Is Not Allowed" );
+ $config = new JConfig();
+?>
+
+<p><strong>Hello in the world of Jumi!</strong></p>
+<p>This is the default Jumi demo file presented on <b><?php echo $config->sitename; ?></b>.</p>
+<p>Jumi is a set of custom code extensions for CMS Joomla! Jumi comes as a component, a plugin and a module.</p>
+<p>Jumi brings you sufficient power, flexibility and simplicity for quick development and reliable operation of your applications running under Joomla!</p>
+<p>Jumi resources: <a href="http://jumi.vedeme.cz">Downloads & guides</a>, <a href="http://edo.webmaster.am/jumi">Tips & tricks</a>.</p>
