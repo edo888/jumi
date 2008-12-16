7a10df8b29ea91a5886d97a1e367848b6fa0e727
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2008-12-16 13:41:56 +0000

    Pack release with new plugin and features: 1. Syntax of Jumi plugin changed to {jumi stored_code_source}code_written{/jumi} 2. Added Jumi editor-xtd (button) plugin that encodes/decodes code_written 3. Jumi plugin processes html, php, js, css code added directly into Joomla! articles by Jumi button

---
+++ admin/plugin_content/jumi/class.jumicoder.php
@@ -0,0 +1,41 @@
+﻿<?php
+/**
+* @version $Id$
+* @package Joomla! 1.5.x, Jumi editors-xtd plugin
+* @copyright (c) 2008 Martin Hajek
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+*
+*/
+
+class JumiCoder
+{
+	function encode($source)
+	{
+		return htmlspecialchars(stripslashes($source), ENT_NOQUOTES);
+	}
+	
+	function decode($source)
+	{
+		if (!function_exists("htmlspecialchars_decode")) {
+			return strtr(stripcslashes($source), array_flip(get_html_translation_table(HTML_SPECIALCHARS, ENT_NOQUOTES))); //for PHP 4
+		} else {
+    	return htmlspecialchars_decode(stripcslashes($source), ENT_NOQUOTES);
+    }
+	}
+		
+	function viewEntities($source)
+	{ //just replaces & to &amp; so that &xxx; is seen by a browser as an entity
+		return str_replace('&', '&amp;', $source);
+	}
+	
+	function cleanRubbish($source)
+	{ // cleans from the $source (encoded code in an article) possible rubbish brought by wysiwyg
+		$cleaningTab = array(	"<br>" => "\n", "<br />" => "\n",	"<p>" => "\n", "</p>" => "\n", "&nbsp;" => " ");
+		foreach ($cleaningTab as $key => $cleaningTab) {
+    	$source = str_replace($key, $cleaningTab[$key], $source);
+		}
+		return $source;
+	}	
+}
+
+?>
