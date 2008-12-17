b2068092e251fcde088136adcf16d11861c893ff
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2008-12-17 15:16:26 +0000

    Bug removed: Jumicoder button did not work in frontend.

--- admin/plugin_editor-xtd/jumicoder.php
+++ admin/plugin_editor-xtd/jumicoder.php
@@ -15,9 +15,12 @@ class plgButtonJumicoder extends JPlugin
 	{
 	  //button image
 		$doc=& JFactory::getDocument();
-		$doc->addStyleDeclaration(".button2-left .jumicoder 	{ background: url(../plugins/editors-xtd/jumicoder/jumicoder.png) 100% 0 no-repeat; }");
+		$doc->addStyleDeclaration('.button2-left .jumicoder 	{ background: url('.JURI::root().'plugins/editors-xtd/jumicoder/jumicoder.png) 100% 0 no-repeat; }');
 		//jumicoder form
-		$link = '../plugins/editors-xtd/jumicoder/jumicoder_form.php';
+		$link = 'plugins/editors-xtd/jumicoder/jumicoder_form.php';
+		if (strstr(JURI::base(),'administrator')) { //joomla prepends JURI::base() to $link automatically
+			$link = '../'.$link;
+		}
 		//modal object with jumicoder form
 		$button = new JObject();
 		$button->set('modal', true);
