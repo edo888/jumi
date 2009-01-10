2b9f4b510da90b1ede5822ab8abdf69cf8ead8a9
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2009-01-10 12:48:38 +0000

    a) Implemented debug mode: if No then module does NOT show any Jumi error messages. b) Issue 13 solved for module: it does not show component database record that is disabled or user has insufficient access rights. In that case there is a warning (if debug mode is on) or nothing (if debug is off).

--- admin/module/helper.php
+++ admin/module/helper.php
@@ -29,9 +29,9 @@ class modJumiHelper
 	}	
 	function getCodeStored($source){ //returns code stored in the database or null.
 		$database  = &JFactory::getDBO();
-		//$user      = &JFactory::getUser();
-		//$database->setQuery("select custom_script from #__jumi where id = '{$source}' and access <= {$user->gid} and published = 1");
-		$database->setQuery("select custom_script from #__jumi where id = $source");
+		$user      = &JFactory::getUser();
+		$database->setQuery("select custom_script from #__jumi where id = '{$source}' and access <= {$user->gid} and published = 1");
+		//$database->setQuery("select custom_script from #__jumi where id = $source"); //all records, all users
 		return $database->loadResult();
 	}
 }
--- admin/module/mod_jumi.php
+++ admin/module/mod_jumi.php
@@ -2,7 +2,7 @@
 /**
 * @version $Id$
 * @package Joomla! 1.5
-* @copyright (c) 2008 Martin Hajek
+* @copyright (c) 2006 - 2009 Martin Hajek
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
 */
 
@@ -12,6 +12,7 @@ defined('_JEXEC') or die('Restricted access');
 // Include the functions only once
 require_once(dirname(__FILE__).DS.'helper.php');
 
+$debug	= $params->get('debug_mode', '0');
 $code_written   = modJumiHelper::getCodeWritten($params); //code written or ""
 $storage_source = modJumiHelper::getStorageSource($params); //filepathname or record id or ""
 
