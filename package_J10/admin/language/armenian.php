50c8ce6402a60e43d86cd56e1562b4ce55a7dc4f
edo888 <edo888@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2009-01-11 11:33:17 +0000

    Jumi Package for Joomla! 1.0.x It contains mod_jumi 1.2.0 and plg_jumi 1.2.0

---
+++ package_J10/admin/language/armenian.php
@@ -0,0 +1,16 @@
+<?php
+# $Id$
+# Note : All ini files need to be saved as UTF-8 - No BOM
+
+define('_JUMI_DETAILS', 'Հավելվածի մարամասները');
+define('_JUMI_INCLFILE', 'Ներբեռնվող ֆայլի ճանապարհի անվանումը, որը պետք է կատարվի խմբագրվող սենարից հետո: Եթե ներբեռնված ֆայլում օգտագրծում եք Jumi արգումենտներ, ապա վերը սահմանեք $jumi[] մասսիվը խմբագրվող սենարում:');
+define('_JUMI_CUSTOMSCRIPT', 'Այստեղ գրեք ցանկացած php, html կամ javascript սցենար, որը պետք է ներբեռնվի:');
+define('_JUMI_NEEDTITLE', 'Հավելվածը պետք է ունենա վերնագիր');
+define('_JUMI_NEEDSCRIPT', 'Դուք պետք է սահմանեք սցենարը պարունակող ֆայլի ճանապարհը կամ գրեք սցենարը:');
+define('_JUMI_TITLE', 'Վերնագիր');
+define('_JUMI_PATH', 'Ճանապարհ');
+define('_JUMI_JUMI APPLICATIONS MANAGER', 'Jumi հավելվածների կառավարիչ');
+define('_JUMI_EDIT APPLICATION', 'Խմբագրել հավելվածը');
+define('_JUMI_CUSTOM SCRIPT', 'Խմբագրվող սցենար');
+define('_JUMI_PATHNAME', 'Ճանապարհի անվանումը');
+define('_JUMI_JUMI APPLICATION', 'Jumi Հավելված');
