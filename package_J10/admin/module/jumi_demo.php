b8592ce5c1681dee859a87ea53318f1e44c66049
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2009-02-01 15:14:44 +0000

    Jumi module 2.0.1d for J1.5. With respect to the 2.0.1 module for J1.5 there are some changes: + debug mode + reading only allowed Jumi database records - language files

--- package_J10/admin/module/jumi_demo.php
+++ package_J10/admin/module/jumi_demo.php
@@ -1,32 +1,21 @@
 <?php
 /**
 * @version $Id$
-* @package Joomla! 1.0.X or Joomla! 1.5.X or Mambo_4.5.X, mod_jumi or plugin_jumi
+* @package Jumi
 * @copyright (C) 2008 Martin Hajek
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
-*/  
+*/
 defined('_JEXEC') OR defined('_VALID_MOS') OR die( "Direct Access Is Not Allowed" );
 ?>
 
 <!-- Intro -->
 <p><strong>Hello in the world of Jumi!</strong></p>
-<p>This is the default Jumi demo file "jumi_demo.php".</p>
-<p>For quick introduction to Jumi read <a href="http://jumi.vedeme.cz/index.php?option=com_content&view=article&id=39&Itemid=37" title="Concise guide">Concise guide to Jumi</a>.</p>
-<!-- Argument values passinbg -->
-<p>The real strength and great flexibility of Jumi lies in its ability to pass practically any number of argument values into your php scripts very easily. Values can be referrenced by $jumi[] array in your code.</p>
-<p>Your current content of $jumi[] array is:</p>
-<?php
-   if (!empty($jumi)){
-      echo "<ul>\n";
-      foreach ($jumi as $key => $value) {
-   	  echo '<li>$jumi['.$key.'] = '.$value.'</li>';
-   	}
-   	echo "</ul>\n";
-	}
-	else {
-		echo "<p>empty</p>";
-	}
-?>
-<!-- End -->
-<p>For another Jumi extensions, demo files, tips and tricks visit<p>
-<div align="center"><a href="http://jumi.vedeme.cz" title="Jumi website"><strong>jumi.vedeme.cz</strong></a></div>
+<p>This is the default Jumi demo file presented on <b><?php echo $mainframe->getCfg('live_site'); ?></b>.</p>
+<p>Jumi is a set of custom code extensions for CMS Joomla! Jumi comes as a component, a plugin and a module.</p>
+<p>Jumi brings you sufficient power, flexibility and simplicity for quick development and reliable operation of your applications running under Joomla!</p>
+<div>Jumi resources:
+	<ul>
+		<li><a href="http://jumi.vedeme.cz">Downloads & guides</a>,</li>
+		<li><a href="http://edo.webmaster.am/jumi">Tips & tricks</a>.</li>
+	</ul>
+</div>
