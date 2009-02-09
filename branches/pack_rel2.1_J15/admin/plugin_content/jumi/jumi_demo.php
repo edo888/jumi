3149c1fed866beeb2694f417277b3486ebee66f6
martin2hajek <martin2hajek@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2009-02-09 17:50:16 +0000

    Changing demo - deleting $jumi array that will not be used in this plugin.

--- branches/pack_rel2.1_J15/admin/plugin_content/jumi/jumi_demo.php
+++ branches/pack_rel2.1_J15/admin/plugin_content/jumi/jumi_demo.php
@@ -4,24 +4,18 @@
 * @package Jumi
 * @copyright (C) 2008 Martin Hajek
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
-*/  
-defined('_JEXEC') or die( "Direct Access Is Not Allowed" );
+*/
+defined('_JEXEC') OR defined('_VALID_MOS') OR die( "Direct Access Is Not Allowed" );
 ?>
+
+<!-- Intro -->
 <p><strong>Hello in the world of Jumi!</strong></p>
-<p>This is the default Jumi demo file.</p>
-<p>Your current content of $jumi[] array is:</p>
-<?php
-   if (!empty($jumi)){
-      echo "<ul>\n";
-      foreach ($jumi as $key => $value) {
-   	  echo '<li>$jumi['.$key.'] = '.$value.'</li>';
-   	}
-   	echo "</ul>\n";
-	}
-	else {
-		echo "<p>empty</p>";
-	}
-?>
+<p>This is the default Jumi demo file presented on <b><?php echo $mainframe->getCfg('live_site'); ?></b>.</p>
 <p>Jumi is a set of custom code extensions for CMS Joomla! Jumi comes as a component, a plugin and a module.</p>
 <p>Jumi brings you sufficient power, flexibility and simplicity for quick development and reliable operation of your applications running under Joomla!</p>
-<p>Jumi resources: <a href="http://jumi.vedeme.cz">Downloads & guides</a>, <a href="http://edo.webmaster.am/jumi">Tips & tricks</a>.</p>
+<div>Jumi resources:
+	<ul>
+		<li><a href="http://jumi.vedeme.cz">Downloads & guides</a>,</li>
+		<li><a href="http://edo.webmaster.am/jumi">Tips & tricks</a>.</li>
+	</ul>
+</div>
