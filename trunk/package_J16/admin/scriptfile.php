dad6d735171b012760fafc19c052a54dbcd8b7b1
edo888 <edo888@46b1b8f6-a0ec-11dd-932a-8f5888711c47>
2011-01-14 13:56:52 +0000

    J16 initial version

---
+++ trunk/package_J16/admin/scriptfile.php
@@ -0,0 +1,116 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class com_JumiInstallerScript {
+
+    /**
+     * method to install the component
+     *
+     * @return void
+     */
+    function install($parent) {
+        // $parent is the class calling this method
+        //$parent->getParent()->setRedirectURL('index.php?option=com_helloworld');
+
+        // installing module
+        $module_installer = new JInstaller;
+        if($module_installer->install(dirname(__FILE__).DS.'module'))
+            echo 'Module install success', '<br />';
+        else
+            echo 'Module install failed', '<br />';
+
+        // installing plugin
+        $plugin_installer = new JInstaller;
+        if($plugin_installer->install(dirname(__FILE__).DS.'plugin'))
+            echo 'Plugin install success', '<br />';
+        else
+            echo 'Plugin install failed', '<br />';
+
+        // installing router
+        $plugin_installer = new JInstaller;
+        if($plugin_installer->install(dirname(__FILE__).DS.'router'))
+            echo 'Router install success', '<br />';
+        else
+            echo 'Router install failed', '<br />';
+
+        // enabling plugin
+        $db =& JFactory::getDBO();
+        $db->setQuery('update #__plugins set published = 1 where element = "jumi" and folder = "content"');
+        $db->query();
+
+        // enabling router
+        $db->setQuery('update #__plugins set published = 1, ordering = 100 where element = "jumirouter" and folder = "system"');
+        $db->query();
+    }
+
+    /**
+     * method to uninstall the component
+     *
+     * @return void
+     */
+    function uninstall($parent) {
+        // $parent is the class calling this method
+        //echo '<p>' . JText::_('COM_HELLOWORLD_UNINSTALL_TEXT') . '</p>';
+
+        $uninstaller = new InstallerController;
+
+        // uninstalling jumi module
+        $db->setQuery("select id from #__modules where title = 'Jumi'");
+        $jumi_module = $db->loadObject();
+        $module_uninstaller = new JInstaller;
+        if($module_uninstaller->uninstall('module', $jumi_module->id))
+            echo 'Module uninstall success', '<br />';
+        else
+            echo 'Module uninstall failed', '<br />';
+
+        // uninstalling jumi plugin
+        $db->setQuery("select id from #__plugins where name = 'Jumi'");
+        $jumi_plugin = $db->loadObject();
+        $plugin_uninstaller = new JInstaller;
+        if($plugin_uninstaller->uninstall('plugin', $jumi_plugin->id))
+            echo 'Plugin uninstall success', '<br />';
+        else
+            echo 'Plugin uninstall failed', '<br />';
+
+        // uninstalling jumi router
+        $db->setQuery("select id from #__plugins where name = 'System - Jumi Router'");
+        $jumi_router = $db->loadObject();
+        $plugin_uninstaller = new JInstaller;
+        if($plugin_uninstaller->uninstall('plugin', $jumi_router->id))
+            echo 'Router uninstall success', '<br />';
+        else
+            echo 'Router uninstall failed', '<br />';
+    }
+
+    /**
+     * method to update the component
+     *
+     * @return void
+     */
+    function update($parent) {
+        // $parent is the class calling this method
+        //echo '<p>' . JText::_('COM_HELLOWORLD_UPDATE_TEXT') . '</p>';
+    }
+
+    /**
+     * method to run before an install/update/uninstall method
+     *
+     * @return void
+     */
+    function preflight($type, $parent) {
+        // $parent is the class calling this method
+        // $type is the type of change (install, update or discover_install)
+        //echo '<p>' . JText::_('COM_HELLOWORLD_PREFLIGHT_' . $type . '_TEXT') . '</p>';
+    }
+
+    /**
+     * method to run after an install/update/uninstall method
+     *
+     * @return void
+     */
+    function postflight($type, $parent) {
+        // $parent is the class calling this method
+        // $type is the type of change (install, update or discover_install)
+        //echo '<p>' . JText::_('COM_HELLOWORLD_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
+    }
+}
